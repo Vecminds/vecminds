7f95451ab12c2a0cc248e5472681c8f2ae19daf4
ishwors <hello.ishwors@gmail.com>
2026-05-27 14:01:09 +0545

    feat: add META_PIXEL_ID to integrations and update layout to include it

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import Script from 'next/script'
 import { Manrope, Inter } from 'next/font/google'
 import './globals.css'
-import { GA_MEASUREMENT_ID, GTM_ID } from '@/lib/integrations'
+import { GA_MEASUREMENT_ID, GTM_ID, META_PIXEL_ID } from '@/lib/integrations'
 
 const manrope = Manrope({
   subsets: ['latin'],
@@ -125,6 +125,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','${GTM_ID}');`}
         </Script>
         <Script src="https://assets.calendly.com/assets/external/widget.js" strategy="afterInteractive" />
+        {/* Meta Pixel */}    
+        <Script id="meta-pixel" strategy="afterInteractive">
+                    {`!function(f,b,e,v,n,t,s)                                                                                                
+     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?                                                                            
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};                                                                    
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';                                                                    
+      n.queue=[];t=b.createElement(e);t.async=!0;                                                                                  
+      t.src=v;s=b.getElementsByTagName(e)[0];                                                                                      
+      s.parentNode.insertBefore(t,s)}(window,document,'script',                                                                    
+      'https://connect.facebook.net/en_US/fbevents.js');                                                                                  
+      fbq('init','${META_PIXEL_ID}');                                                                                                     
+      fbq('track','PageView');`}
+                </Script>
+                <noscript> {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img height="1" width="1"
+                        style={{display: 'none'}}
+                        src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+                        alt=""/>
+                </noscript>
         {children}
       </body>
     </html>
--- lib/integrations.ts
+++ lib/integrations.ts
@@ -1,3 +1,4 @@
 export const CALENDLY_URL = 'https://calendly.com/vecminds/vecminds-1-1'
 export const GA_MEASUREMENT_ID = 'G-3VPTT945Y7'
 export const GTM_ID = 'GT-MK9LZ4VV'
+export const META_PIXEL_ID = '1622059085341763'
